3f36efbfd8b784a2bb3bd8473a50be885b2eb28d
@maxgfr <maxgolfier@gmail.com>
2020-09-13 17:53:46 +0200

    push

--- back-end/index.js
+++ back-end/index.js
@@ -1,38 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const dotenv = require('dotenv').config();
-const {
-  v4: uuidv4
-} = require('uuid');
-const ApiMoney = require('api-money-node-sdk');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const dotenv = require("dotenv").config();
+const { v4: uuidv4 } = require("uuid");
+const { ApiMoney } = require("api-money-node-sdk");
 const app = express();
 const port = 3000;
 const client = new ApiMoney({
-  secret_key: process.env.SECRET_KEY,
-  access_key: process.env.ACCESS_KEY,
+  secretKey: process.env.SECRET_KEY,
+  accessKey: process.env.ACCESS_KEY,
   mode: process.env.MODE,
-  version: 1
+  version: 1,
 });
 
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({
-  extended: true
-})); // parse application/x-www-form-urlencoded
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(cors());
 
-app.get('/', (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.json({
-    success: true
+    success: true,
   });
-})
+});
 
-app.post('/createAccount', (req, res, next) => {
-  client.request('POST', '/accounts/standard', {
+app.post("/createAccount", (req, res, next) => {
+  client
+    .request("POST", "/accounts/standard", {
       subscriber: {
         lastname: req.body.lastname,
         firstname: req.body.firstname,
@@ -42,275 +43,331 @@ app.post('/createAccount', (req, res, next) => {
         nationality: req.body.nationality,
         citizen_us: req.body.citizen_us,
         fiscal_us: req.body.fiscal_us,
-        fiscal_out_france: req.body.fiscal_out_france
+        fiscal_out_france: req.body.fiscal_out_france,
       },
       address: {
         label1: req.body.label1,
         zip_code: req.body.zip_code,
         city: req.body.city,
-        country: req.body.country
+        country: req.body.country,
       },
       email: req.body.email,
     })
-    .then((response) => {
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.get('/getAccounts', (req, res, next) => {
-  client.request('GET', '/accounts', {})
-    .then((response) => {
+app.get("/getAccounts", (req, res, next) => {
+  client
+    .request("GET", "/accounts", {})
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/getAccount', (req, res, next) => {
-  client.request('GET', '/accounts/' + req.body.id, {})
-    .then((response) => {
+app.post("/getAccount", (req, res, next) => {
+  client
+    .request("GET", "/accounts/" + req.body.id, {})
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/createWallet', (req, res, next) => {
-  client.request('POST', '/wallets/', {
+app.post("/createWallet", (req, res, next) => {
+  client
+    .request("POST", "/wallets/", {
       account_id: req.body.account_id,
-      type: req.body.type
-    })
-    .then((response) => {
-      res.json({
-        url: response
-      })
-    })
-    .catch((error) => {
-      res.json({
-        err: error.response.data
-      });
-    });
-});
-
-
-app.post('/getWallet', (req, res, next) => {
-  client.request('GET', '/wallets/' + req.body.id, {})
-    .then((response) => {
-      res.json({
-        url: response
-      })
-    })
-    .catch((error) => {
-      res.json({
-        err: error.response.data
-      });
-    });
-});
-
-
-app.post('/getWalletBalance', (req, res, next) => {
-  client.request('GET', '/wallets/' + req.body.id + "/balance", {})
-    .then((response) => {
-      res.json({
-        url: response
-      })
-    })
-    .catch((error) => {
-      res.json({
-        err: error.response.data
-      });
-    });
-});
-
-app.post('/getWalletActivities', (req, res, next) => {
-  client.request('GET', `/wallets/${req.body.id}/activities/?per_page=${req.body.per_page}&page=${req.body.page}`, {})
-    .then((response) => {
-      res.json({
-        url: response
-      })
-    })
-    .catch((error) => {
-      res.json({
-        err: error.response.data
-      });
-    });
-});
-
-app.post('/getWalletActivity', (req, res, next) => {
-  client.request('GET', '/wallets/' + req.body.id + "/activities/" + req.body.activity_id, {})
-    .then((response) => {
-      res.json({
-        url: response
-      })
-    })
-    .catch((error) => {
-      res.json({
-        err: error.response.data
-      });
-    });
-});
-
-app.post('/addDocument', (req, res, next) => {
-  client.request('POST', '/accounts/' + req.body.account_id + '/documents', {
       type: req.body.type,
-      files: req.body.files
+    })
+    .then(response => {
+      res.json({
+        url: response,
+      });
+    })
+    .catch(error => {
+      res.json({
+        err: error,
+      });
+    });
+});
+
+app.post("/getWallet", (req, res, next) => {
+  client
+    .request("GET", "/wallets/" + req.body.id, {})
+    .then(response => {
+      res.json({
+        url: response,
+      });
+    })
+    .catch(error => {
+      res.json({
+        err: error,
+      });
+    });
+});
+
+app.post("/getWalletBalance", (req, res, next) => {
+  client
+    .request("GET", "/wallets/" + req.body.id + "/balance", {})
+    .then(response => {
+      res.json({
+        url: response,
+      });
+    })
+    .catch(error => {
+      res.json({
+        err: error,
+      });
+    });
+});
+
+app.post("/getWalletActivities", (req, res, next) => {
+  client
+    .request(
+      "GET",
+      `/wallets/${req.body.id}/activities/?per_page=${req.body.per_page}&page=${req.body.page}`,
+      {}
+    )
+    .then(response => {
+      res.json({
+        url: response,
+      });
+    })
+    .catch(error => {
+      res.json({
+        err: error,
+      });
+    });
+});
+
+app.post("/getWalletActivity", (req, res, next) => {
+  client
+    .request(
+      "GET",
+      "/wallets/" + req.body.id + "/activities/" + req.body.activity_id,
+      {}
+    )
+    .then(response => {
+      res.json({
+        url: response,
+      });
+    })
+    .catch(error => {
+      res.json({
+        err: error,
+      });
+    });
+});
+
+app.post("/addDocument", (req, res, next) => {
+  client
+    .request("POST", "/accounts/" + req.body.account_id + "/documents", {
+      type: req.body.type,
+      files: req.body.files,
       // files: [{
       //   file_name: req.body.file_name,
       //   content: req.body.content
       // }]
     })
-    .then((response) => {
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/getDocument', (req, res, next) => {
-  client.request('GET', '/accounts/' + req.body.account_id + '/documents/' + req.body.document_id, {})
-    .then((response) => {
+app.post("/getDocument", (req, res, next) => {
+  client
+    .request(
+      "GET",
+      "/accounts/" + req.body.account_id + "/documents/" + req.body.document_id,
+      {}
+    )
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/askKyc', (req, res, next) => {
-  client.request('PUT', '/accounts/' + req.body.account_id + '/validations/' + req.body.kyc_level + '/submit', {})
-    .then((response) => {
+app.post("/askKyc", (req, res, next) => {
+  client
+    .request(
+      "PUT",
+      "/accounts/" +
+        req.body.account_id +
+        "/validations/" +
+        req.body.kyc_level +
+        "/submit",
+      {}
+    )
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/getLastKyc', (req, res, next) => {
-  client.request('GET', '/accounts/' + req.body.account_id + '/validations/last', {})
-    .then((response) => {
+app.post("/getLastKyc", (req, res, next) => {
+  client
+    .request(
+      "GET",
+      "/accounts/" + req.body.account_id + "/validations/last",
+      {}
+    )
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-
-app.post('/getTransaction', (req, res, next) => {
-  client.request('GET', '/transactions/' + req.body.id, {})
-    .then((response) => {
+app.post("/getTransaction", (req, res, next) => {
+  client
+    .request("GET", "/transactions/" + req.body.id, {})
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-
-app.post('/refundTransaction', (req, res, next) => {
-  client.request('PUT', '/transactions/' + req.body.id + '/refund', {})
-    .then((response) => {
-      res.json({
-        url: response
-      })
+app.post("/refundTransaction", (req, res, next) => {
+  client
+    .request("PUT", "/transactions/" + req.body.id + "/refund", {
+      amount: req.body.amount,
     })
-    .catch((error) => {
+    .then(response => {
       res.json({
-        err: error.response.data
+        url: response,
+      });
+    })
+    .catch(error => {
+      res.json({
+        err: error,
       });
     });
 });
 
-
-app.get('/receiptTransaction', (req, res, next) => {
-  client.request('GET', '/transactions/' + req.body.id + '/receipt' + req.body.language, {})
-    .then((response) => {
+app.post("/confirmTransaction", (req, res, next) => {
+  client
+    .request("PUT", "/cash-in/" + req.body.id, {})
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.get('/partnerTransaction', (req, res, next) => {
-  client.request('GET', '/transactions/partner_ref/' + req.body.partner_ref, {})
-    .then((response) => {
+app.post("/receiptTransaction", (req, res, next) => {
+  client
+    .request(
+      "GET",
+      "/transactions/" + req.body.id + "/receipt" + req.body.language,
+      {}
+    )
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/transfers', (req, res, next) => {
-  client.request('POST', '/transfers', {
+app.post("/partnerTransaction", (req, res, next) => {
+  client
+    .request("GET", "/transactions/partner_ref/" + req.body.partner_ref, {})
+    .then(response => {
+      res.json({
+        url: response,
+      });
+    })
+    .catch(error => {
+      res.json({
+        err: error,
+      });
+    });
+});
+
+app.post("/transfers", (req, res, next) => {
+  client
+    .request("POST", "/transfers", {
       partner_ref: req.body.partner_ref,
       sender_wallet_id: req.body.sender_wallet_id,
       receiver_wallet_id: req.body.receiver_wallet_id,
       amount: req.body.amount,
     })
-    .then((response) => {
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-
-app.post('/card', (req, res, next) => {
-  client.request('POST', '/cash-in/creditcards/init', {
+app.post("/card", (req, res, next) => {
+  client
+    .request("POST", "/cash-in/creditcards/init", {
       partner_ref: req.body.partner_ref,
       receiver_wallet_id: req.body.receiver_wallet_id,
       fees_wallet_id: req.body.fees_wallet_id,
@@ -318,22 +375,23 @@ app.post('/card', (req, res, next) => {
       fees: req.body.fees,
       return_url: req.body.return_url,
       lang: req.body.lang,
-      auth_timeout_delay: 86400
+      auth_timeout_delay: 86400,
     })
-    .then((response) => {
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/transfers-with-card', (req, res, next) => {
-  client.request('POST', '/cash-in/creditcards/' + req.body.card_id, {
+app.post("/transfers-with-card", (req, res, next) => {
+  client
+    .request("POST", "/cash-in/creditcards/" + req.body.card_id, {
       partner_ref: req.body.partner_ref,
       payment_method: "CREDIT_CARD",
       receiver_wallet_id: req.body.receiver_wallet_id,
@@ -342,43 +400,44 @@ app.post('/transfers-with-card', (req, res, next) => {
       fees: req.body.fees,
       return_url: req.body.return_url,
       lang: req.body.lang,
-      auth_timeout_delay: 86400
+      auth_timeout_delay: 86400,
     })
-    .then((response) => {
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/authorize-bank-transfer', (req, res, next) => {
-  client.request('POST', '/cash-in/bankwire/authorize', {
+app.post("/authorize-bank-transfer", (req, res, next) => {
+  client
+    .request("POST", "/cash-in/bankwire/authorize", {
       partner_ref: req.body.partner_ref,
       receiver_wallet_id: req.body.receiver_wallet_id,
       fees_wallet_id: req.body.fees_wallet_id,
       amount: req.body.amount,
-      fees: req.body.fees
+      fees: req.body.fees,
     })
-    .then((response) => {
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-
-app.post('/cash-out', (req, res, next) => {
-  client.request('POST', '/cash-out', {
+app.post("/cash-out", (req, res, next) => {
+  client
+    .request("POST", "/cash-out", {
       partner_ref: req.body.partner_ref,
       sender_wallet_id: req.body.sender_wallet_id,
       fees_wallet_id: req.body.fees_wallet_id,
@@ -386,91 +445,112 @@ app.post('/cash-out', (req, res, next) => {
       fees: req.body.fees,
       bankaccount_id: req.body.bankaccount_id,
     })
-    .then((response) => {
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/createBankAccount', (req, res, next) => {
-  client.request('POST', '/bankaccounts', {
+app.post("/createBankAccount", (req, res, next) => {
+  client
+    .request("POST", "/bankaccounts", {
       account_id: req.body.account_id,
       number: req.body.number,
       holder_name: req.body.holder_name,
     })
-    .then((response) => {
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-
-app.post('/getBankAccount', (req, res, next) => {
-  client.request('GET', '/bankaccounts/' + req.body.id, {})
-    .then((response) => {
+app.post("/getBankAccount", (req, res, next) => {
+  client
+    .request("GET", "/bankaccounts/" + req.body.id, {})
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/deleteBankAccount', (req, res, next) => {
-  client.request('DELETE', '/bankaccounts/' + req.body.id, {})
-    .then((response) => {
+app.post("/deleteBankAccount", (req, res, next) => {
+  client
+    .request("DELETE", "/bankaccounts/" + req.body.id, {})
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/getCreditCard', (req, res, next) => {
-  client.request('GET', '/creditcards/' + req.body.id, {})
-    .then((response) => {
+app.post("/listCreditCard", (req, res, next) => {
+  client
+    .request("GET", "/creditcards/?account_id=" + req.body.account_id, {})
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.post('/deleteCreditCard', (req, res, next) => {
-  client.request('DELETE', '/creditcards/' + req.body.id, {})
-    .then((response) => {
+app.post("/getCreditCard", (req, res, next) => {
+  client
+    .request("GET", "/creditcards/" + req.body.id, {})
+    .then(response => {
       res.json({
-        url: response
-      })
+        url: response,
+      });
     })
-    .catch((error) => {
+    .catch(error => {
       res.json({
-        err: error.response.data
+        err: error,
       });
     });
 });
 
-app.listen(port, () => console.log(`The application is listening on port ${port}!`))
+app.post("/deleteCreditCard", (req, res, next) => {
+  client
+    .request("DELETE", "/creditcards/" + req.body.id, {})
+    .then(response => {
+      res.json({
+        url: response,
+      });
+    })
+    .catch(error => {
+      res.json({
+        err: error,
+      });
+    });
+});
+
+app.listen(port, () =>
+  console.log(`The application is listening on port ${port}!`)
+);
